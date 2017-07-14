3fdcf25ad2104731d05e12bc4bc31e7c96616371
Ryan Flaherty <ryan.flaherty.71@gmail.com>
2017-07-13 22:18:34 -0500

    Setup basic server

--- src/server.js
+++ src/server.js
@@ -10,10 +10,70 @@ app.use(express.static("public/"));
 server.listen(8000, () => console.log("listening on *:8000"));
 
 let current_video = {
-	url:        "",
-	time:       0,
-	is_playing: false
+	url:          "",
+	current_time: 0,
+	total_time:   0,
+	is_playing:   false
 };
+
+function video_change(url) {
+	// @todo: Get video info from YouTube
+	
+	// http.get("http://www.youtube.com/oembed?url=${data.link}&format=json",
+	// 	response => {
+	// 		let data = [];
+	// 		response.on("data", chunk => data.push(chunk));
+
+	// 		response.on("end", () => {
+	// 			if (r.statusCode == 200) {
+	// 				data = JSON.parse(data);
+	// 				console.log(data);
+
+	// 				let new_video = {
+
+	// 				};
+
+	// 				video_queue.push(new_video);
+	// 			}
+	// 			else {
+	// 				console.error("Failed to queue video, could not retrieve video info from YouTube");
+	// 			}
+	// 		});
+	// 	})
+	// 	.end();
+
+	current_video.url          = url;
+	current_video.current_time = 0;
+	current_video.total_time   = 0;
+
+	video_play();
+}
+
+function video_play() {
+	if (current_video.is_playing) {
+		console.error("Failed to play video, a video is already playing");
+		return;
+	}
+
+	current_video.is_playing = true;
+}
+
+function video_pause() {
+	if (!current_video.is_playing) {
+		console.error("Failed to pause video, a video is not playing");
+		return;
+	}
+
+	current_video.is_playing = false;
+}
+
+function video_seek(time) {
+	current_video.time = data.time;
+
+	if (!current_video.is_playing) {
+		current_video.is_playing = true;
+	}
+}
 
 let users = [];
 
@@ -74,47 +134,8 @@ socket.on("connection", client => {
 		user.name = data.name;
 	});
 	
-	client.on("change-video", data => {
-		
-	});
-
-	client.on("play-video", data => {
-		if (!current_video.is_playing) {
-			console.error("Failed to play video, a video is already playing");
-			return;
-		}
-
-		current_video.is_playing = true;
-	});
-
-	client.on("pause-video", data => {
-
-	});
-
-	client.on("seek-to", data => {
-
-	});
+	client.on("video-change", data => video_change(data.url));
+	client.on("video-play",   data => video_play());
+	client.on("video-pause",  data => video_pause());
+	client.on("video-seek",   data => video_seek(data.time));
 });
-
-// http.get("http://www.youtube.com/oembed?url=${data.link}&format=json",
-// 	response => {
-// 		let data = [];
-// 		response.on("data", chunk => data.push(chunk));
-
-// 		response.on("end", () => {
-// 			if (r.statusCode == 200) {
-// 				data = JSON.parse(data);
-// 				console.log(data);
-
-// 				let new_video = {
-
-// 				};
-
-// 				video_queue.push(new_video);
-// 			}
-// 			else {
-// 				console.error("Failed to queue video, could not retrieve video info from YouTube");
-// 			}
-// 		});
-// 	})
-// 	.end();
