28d8c54a9cd467a851b52ed49415046845ac1af6
Ryan Flaherty <ryan.flaherty.71@gmail.com>
2017-07-13 22:06:30 -0500

    Revamp server

--- src/server.js
+++ src/server.js
@@ -1,80 +1,120 @@
-var http = require("http");
-var express = require("express");
-var app = express();
-var server = http.Server(app);
-var io = require("socket.io");
-var socket = io(server);
-var connected_count = 0;
+let http    = require("http");
+let express = require("express");
+let io      = require("socket.io");
 
-socket.on("connection", function(c) {
-	connected_count++;
-	console.log("Client connected. Active sockets: " + connected_count);
-
-	c.on("disconnect", function() {
-		connected_count--;
-		console.log("Client disconnected. Active sockets: " + connected_count);
-	});
-
-	c.on("video-cursor-changed", function(data) {
-		c.emit("change-video-position", {  });
-	});
-
-	c.on("should-play-video", function(data) {
-		console.log("should-play-the-vidya");
-		c.broadcast.emit("play-video", { time: data.time }); 
-
-	});
-
-	c.on("should-load-video", function(data) {
-		http.get("http://www.youtube.com/oembed?url=" + data.link + "&format=json",
-				r => {
-					var data = [];
-					r.on("data", chunk => data.push(chunk));
-
-					r.on("end", () => {
-						if (r.statusCode == 200) {
-							data = JSON.parse(data);
-							console.log(data);
-							socket.emit("load-video", { 
-								video_id: data.html.substring(68, 79), 
-								time:     0,
-								quality:  "default"
-							});
-							console.log("Emitted from socket");
-						}
-						else {
-							console.error("Failed to retrieve video data, the api returned a status code of '" + r.statusCode + "'");
-						}
-					});
-				})
-				.end();
-
-		//todo(Matt): Store queued videos and then broadcast to any clients that join the room
-		// get the video ID from the returned data
-		// possibly use the html to just inject it in
-
-	});
-
-	c.on("should-queue-video", function(data) {
-		c.broadcast.emit("queue-video", { 
-			video_id: data.video_id, 
-			time:     data.time,
-			quality:  data.quality
-		});
-	});
-
-	c.on("should-pause-video", function(data) {
-		c.broadcast.emit("pause-video", { time: data.time });
-	});
-});
-
+let app    = express();
+let server = http.Server(app);
+let socket = io(server);
 
 app.use(express.static("public/"));
+server.listen(8000, () => console.log("listening on *:8000"));
 
-server.listen(8000, function() {
-	console.log('listening on *:8000');
+let current_video = {
+	url:        "",
+	time:       0,
+	is_playing: false
+};
+
+let users = [];
+
+function find_user(id) {
+	let user = null;
+	for (let u of users) {
+		if (u.id != client.id) continue;
+
+		user = u;
+		break;
+	}
+
+	return user;
+}
+
+socket.on("connection", client => {
+	console.log("Client connected");
+	
+	client.emit("initialize", {
+		current_video: {
+			url:  current_video.url,
+			time: current_video.time,
+			is_playing: false
+		},
+		users: users
+	});
+
+	let user = {
+		id: client.id,
+		name: "${client.id}"
+	};
+
+	socket.emit("add-user", { user: user });
+	users.push(user);
+
+	client.on("disconnect", () => {
+		console.log("Client disconnected");
+
+		let user = find_user(client.id);
+		if (!user) {
+			console.error("Failed to remove user on disconnect, a user with id '${client.id}' was not found");
+			return;
+		}
+
+		let index = users.indexOf(user);
+		users.splice(index, 1);
+
+		socket.emit("remove-user", { id: user.id });
+	});
+
+	client.on("set-name", data => {
+		let user = find_user(client.id);
+		if (!user) {
+			console.error("Failed to set user's name, a user with id '${client.id}' was not found");
+			return;
+		}
+
+		user.name = data.name;
+	});
+	
+	client.on("change-video", data => {
+		
+	});
+
+	client.on("play-video", data => {
+		if (!current_video.is_playing) {
+			console.error("Failed to play video, a video is already playing");
+			return;
+		}
+
+		current_video.is_playing = true;
+	});
+
+	client.on("pause-video", data => {
+
+	});
+
+	client.on("seek-to", data => {
+
+	});
 });
 
-server.on("error", function(error) {
-	console.log(error);
-});
+// http.get("http://www.youtube.com/oembed?url=${data.link}&format=json",
+// 	response => {
+// 		let data = [];
+// 		response.on("data", chunk => data.push(chunk));
+
+// 		response.on("end", () => {
+// 			if (r.statusCode == 200) {
+// 				data = JSON.parse(data);
+// 				console.log(data);
+
+// 				let new_video = {
+
+// 				};
+
+// 				video_queue.push(new_video);
+// 			}
+// 			else {
+// 				console.error("Failed to queue video, could not retrieve video info from YouTube");
+// 			}
+// 		});
+// 	})
+// 	.end();
